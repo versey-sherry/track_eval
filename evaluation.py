ca80d75028cee71896297638ad4b17860ae31c3e
versey-sherry <sherrylin42@gmail.com>
2020-09-23 21:00:18 -0700

    Add result example and single evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,9 @@
 #Command line tool for evaluating tracking
 '''
 Input
---prediction_folder path to the txt files of predicted outputs
---video_folder path to the training sequence with gt
+--prediction_dir path to the txt files of predicted outputs
+--video_dir path to the training sequence with gt
+--results_dir folder to save the results
 --evaluation_type single multiple or cell
 --writeout True if video output is needed
 
@@ -27,15 +28,17 @@ number of true positive associations divided by the number of associations in th
 Target effectiveness is computed as the number of the assigned track observations
 over the total number of frames of the target
 
-All video folders should be in the same dir, to process LaSOT
+All video dirs should be in the same dir, to process LaSOT
 find . -maxdepth 2  -print -exec mv {} . \;
 '''
 
 '''
 example
-python evaluation.py --predcition_folder ../results/sot_results/TB-100 \
---prediction video_folder ../data/single_tracking/OTB \
+python evaluation.py --predcition_dir ../results/sot_results/TB-100 \
+--video_dir ../data/single_tracking/TB-100 \
 --evaluation_type single \
+--threshold 0.5 \
+--save_dir \
 --writeout Ture
 
 '''
@@ -131,14 +134,9 @@ def process_multiple(txt_file):
             info[line[0]].append(line[1:])
     return info
 
-# process cell tracking prediction
-# prediction format <frame> <id> <x> <y>
-def process_cellpd(txt_file):
-    info = 
-
 #process C2C12 cell tracking ground truth that uses xml annotation
 #return a dictionary with frame as key and a list of [<id>, <x>, <y>] presented in the frame
-def porcess_cellgt(xml_file):
+def process_cellgt(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
@@ -167,14 +165,90 @@ def porcess_cellgt(xml_file):
     #print(gt.keys())
     return gt
 
+# Evaluation for one video
+# output video name, accuracy and robustness
+'''
+video_dir = 'gt_example/TB-100'
+prediction_dir = 'result_example/sot_results/TB-100/goturn'
+save_dir = './results'
+prediction = os.path.join(prediction_dir, 'Biker.txt')
+gt = os.path.join(video_dir, 'Biker', 'groundtruth_rect.txt')
+'''
+def single_eval(prediction, gt, save_dir, threshold, writeout=False):
+    video_name = prediction.split('/')[-1].split('.')[0]
+    prediction_dict = process_single(prediction)
+    gt_dict = process_single(gt)
 
-def single_eval(prediction, gt, threshold, single=True):
-    #producing evaluation results for single video
-    if single:
-        pass
-    #producing evaluation results for multiple videos
+    if os.path.exists(save_dir):
+        print('Save directory already exists')
     else:
-        pass
+        os.makedirs(save_dir)
+        print('Making new save dir')
+
+    #preparation for writing out the video
+    if writeout:
+        img_dir = os.path.join('/'.join(gt.split('/')[0:-1]), 'img')
+        print(img_dir)
+        video_name = prediction.split('/')[-1].split('.')[0]
+        images = sorted([file for file in os.listdir(img_dir) if '.jpg' in file or '.jpeg' in file])
+        img = cv2.imread(os.path.join(img_dir, images[0]))
+        height, width, _ = img.shape
+        size = (width, height)
+        file_name = file_name = '_'.join([prediction.split('/')[-2], video_name, 'video.mp4'])
+        file_name = os.path.join(save_dir, file_name)
+        print('Video saved to', file_name)
+        out = cv2.VideoWriter(file_name,cv2.VideoWriter_fourcc(*'MP4V'), 20, size)
+
+    acc_list = []
+    robustness = 0
+    frame_num = 1
+
+    #loop through every frame in ground truth
+    while frame_num <= len(gt_dict):
+        prediction = prediction_dict.get(frame_num)[0]
+        gt = gt_dict.get(frame_num)[0]
+
+        prediction = [prediction[0], prediction[1], prediction[0]+prediction[2], prediction[1]+prediction[3]]
+        gt = [gt[0], gt[1], gt[0] + gt[2], gt[1]+gt[3]]
+
+        acc = compute_iou(prediction, gt)
+        acc_list.append(acc)
+
+        if acc > threshold:
+            robustness +=1
+        #print(prediction)
+        #print(gt)
+        
+        #output videos
+        if writeout:
+            img = cv2.imread(os.path.join(img_dir, images[frame_num-1]))
+            print(images[frame_num-1])
+            #ploting prediction with red box
+            img = cv2.rectangle(img, (prediction[0], prediction[1]), (prediction[2], prediction[3]), (0, 0, 255), 2)
+            #ploting ground truth with green box
+            img = cv2.rectangle(img, (gt[0], gt[1]), (gt[2], gt[3]), (0,255,0), 2)
+            #Put stats on frame
+            text = 'Frame {}: IoU is {}%'.format(frame_num,round((acc *100),2))
+            img = cv2.putText(img, text, (30,30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,255,0), 2, cv2.LINE_AA) 
+            out.write(img)
+        
+        frame_num+=1
+    
+    if writeout:
+        out.release()
+
+    if len(acc_list) >0:
+        accuracy = sum(acc_list)/len(acc_list)
+    else:
+        print('No IoU')
+
+    if robustness >0:
+        robustness = robustness/len(gt_dict)
+        
+    return video_name, accuracy, robustness
+
+
+
 
 def multi_eval(predition, gt, threshold,single=True):
     pass
@@ -187,9 +261,12 @@ def visualization(prediction, gt, threshold):
 
 def main():
     parser = argparse.ArgumentParser(description='tracker evaluation')
-    parser.add_argument('--predcition_folder', type=str, help = 'path to the txt files of the predicted outputs')
-    parser.add_argument('--video_folder', type=str, help = 'path to the folders that contains image sequence and gt')
+    parser.add_argument('--predcition_dir', type=str, help = 'path to the txt files of the predicted outputs')
+    parser.add_argument('--video_dir', type=str, help = 'path to the directory that contains image sequence and gt')
+    parser.add_argument('--save_dir', type=str, default= './results')
     parser.add_argument('--evaluation_type', type=str, help= "flag for single mulitple or cell")
+    parser.add_argument('--threshold', type=int, default = 0.5, help= "threshold for correctly tracked")
+    args= parser.parse_args()
 
 
 if __name__ == '__main__':
